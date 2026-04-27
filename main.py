35e8a9b8aa0eab6659c543281ea5f4c3c6cb0b7a
adityaraman27 <2adityaraman@gmail.com>
2026-04-27 13:55:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption
 
+clock = pygame.time.Clock()
+FPS = 60
+
+bg = pygame.image.load('')
+
+
 
 run = True
 while run:
 
+    Clock.tick(FPS)
 
 
     for event in pygame.event.get():
